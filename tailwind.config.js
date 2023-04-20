3614fb03fd6f30d70737e59616eb0212ea659410
Suzaalx <sujalxetry00@gmail.com>
2023-04-20 22:32:22 +0545

    testiomonial grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,22 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+
+
+    fontFamily: {
+      "anuphan": ['Anuphan', 'sans-serif'],
+      "chivo-mono": ['Chivo Mono', 'monospace'],
+      "fraunces": ['Fraunces', 'serif'],
+      "barlow": ['Barlow', 'serif'],
+    },
     extend: {
-      fontFamily: {
-        Barlow: ["Barlow", "serif"],
-        Franunces: ["Franunces"],
-      },
+      
 
       colors: {
         // Primary
-        'background': "hsl(51, 100%, 49%, 0.1)",
+        background: "hsl(51, 100%, 49%, 0.1)",
         "soft-red": "hsl(7, 99%, 70%)",
-        "yellow": "hsl(51, 100%, 49%, 0.5)",
+        yellow: "hsl(51, 100%, 49%, 0.5)",
         "graphic-design-blue": "hsl(167, 40%, 24%)",
         "photography-blue": "hsl(198, 62%, 26%)",
         "footer-blue": "hsl(168, 34%, 41%)",
@@ -23,7 +28,6 @@ module.exports = {
         "dark-grayish-blue": "hsl(232, 10%, 55%)",
         "grayish-blue": "hsl(210, 4%, 67%)",
       },
-  
     },
 
     plugins: [],
