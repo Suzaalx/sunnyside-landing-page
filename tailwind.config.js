d32998524b325aebdcaf577078a7e129defb79cd
Suzaalx <sujalxetry00@gmail.com>
2023-04-21 15:33:08 +0545

    testimonial section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,10 @@ module.exports = {
       "fraunces": ['Fraunces', 'serif'],
       "barlow": ['Barlow', 'serif'],
     },
+
+
     extend: {
       
-
       colors: {
         // Primary
         background: "hsl(51, 100%, 49%, 0.1)",
@@ -21,13 +22,15 @@ module.exports = {
         "graphic-design-blue": "hsl(167, 40%, 24%)",
         "photography-blue": "hsl(198, 62%, 26%)",
         "footer-blue": "hsl(168, 34%, 41%)",
-
+        "footer-bg": "#91D4C7",
+        "footer-text": "rgba(129, 132, 152, 1)",
         // Neutral
         "very-dark-blue": "hsl(212, 27%, 19%)",
         "very-dark-grayish-blue": "hsl(213, 9%, 39%)",
         "dark-grayish-blue": "hsl(232, 10%, 55%)",
         "grayish-blue": "hsl(210, 4%, 67%)",
       },
+      
     },
 
     plugins: [],
